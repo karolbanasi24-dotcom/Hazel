4d78d93063e75f834ad24e62c7be0df2ca59bcb2
karolbanasi24-dotcom <karolbanasi24@gmail.com>
2026-05-31 17:44:01 +0200

    s

---
+++ Sandbox/src/SandboxApp.cpp
@@ -0,0 +1,20 @@
+#include <Hazel.h>
+
+class Sandbox : public Hazel::Application
+{
+public:
+	Sandbox()
+	{
+	}
+
+	~Sandbox()
+	{
+	}
+};
+
+void main() 
+{
+ 	Sandbox* sandbox = new Sandbox();
+	sandbox->Run();
+	delete sandbox;
+}
